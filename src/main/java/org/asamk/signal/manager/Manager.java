1922bf63f004000954e6b5a5d793790635238416
Hermann Höhne <hoehermann@gmx.de>
2020-11-01 20:02:55 +0100

    Make sendReceipt public.

--- src/main/java/org/asamk/signal/manager/Manager.java
+++ src/main/java/org/asamk/signal/manager/Manager.java
@@ -1071,10 +1071,12 @@ public class Manager implements Closeable {
         return sendMessage(messageBuilder, List.of(recipient));
     }
 
-    void sendReceipt(
-            SignalServiceAddress remoteAddress, long messageId
-    ) throws IOException, UntrustedIdentityException {
-        SignalServiceReceiptMessage receiptMessage = new SignalServiceReceiptMessage(SignalServiceReceiptMessage.Type.DELIVERY,
+    public void sendReceipt(SignalServiceAddress remoteAddress, long messageId) throws IOException, UntrustedIdentityException {
+        sendReceipt(remoteAddress, messageId, SignalServiceReceiptMessage.Type.DELIVERY);
+    }
+
+    public void sendReceipt(SignalServiceAddress remoteAddress, long messageId, SignalServiceReceiptMessage.Type type) throws IOException, UntrustedIdentityException {
+        SignalServiceReceiptMessage receiptMessage = new SignalServiceReceiptMessage(type,
                 List.of(messageId),
                 System.currentTimeMillis());
 
